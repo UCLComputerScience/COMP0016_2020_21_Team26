51df3367cae1a9ea1ff192d6115575ee76f352b2
Chris <chris.tomy.19@ucl.ac.uk>
2020-11-23 16:53:02 +0000

    remove unused ListView

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -6,14 +6,12 @@ class SettingsPage extends StatelessWidget { // TODO
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: ListView(children: [
-          TextField(
-            onSubmitted: (String value) {
-              final model = Provider.of<UserModel>(context, listen: false);
-              model.postcodePrefix = value;
-            },
-          ),
-        ],)
+        child: TextField(
+          onSubmitted: (String value) {
+            final model = Provider.of<UserModel>(context, listen: false);
+            model.postcodePrefix = value;
+          },
+        ),
     );
   }
 }
