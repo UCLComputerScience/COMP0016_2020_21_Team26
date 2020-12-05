8338ddb77bcde8ec07f6c87304833cc27bd545ea
saachipahwa <55795994+saachipahwa@users.noreply.github.com>
2020-12-05 19:59:14 +0000

    finished checkup screen

--- lib/checkup.dart
+++ lib/checkup.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:pedometer/pedometer.dart';
 
-//import 'package:fit_kit/fit_kit.dart';
+void main() => runApp(Checkup());
 
 class Checkup extends StatelessWidget {
   // This widget is the root of your application.
@@ -13,42 +14,92 @@ class Checkup extends StatelessWidget {
       home: Scaffold(
           appBar: AppBar(title: const Text("Checkup")),
           body: Column(children: <Widget>[
-            Text("How do you feel right now?"),
-            MyStatefulWidget(),
-            Text("Your steps this week:"),
+            WBSliderWidget(),
+            PedometerWidget()
             //Text(readStepCount().toString())
           ])),
     );
   }
 }
 
-class MyStatefulWidget extends StatefulWidget {
-  MyStatefulWidget({Key key}) : super(key: key);
+//wb scale
+class WBSliderWidget extends StatefulWidget {
+  WBSliderWidget({Key key}) : super(key: key);
 
   @override
-  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+  _WBSliderWidgetState createState() => _WBSliderWidgetState();
 }
 
 /// This is the private State class that goes with MyStatefulWidget.
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+class _WBSliderWidgetState extends State<WBSliderWidget> {
   double _currentSliderValue = 0;
   double _weeklyWBScore = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Slider(
-      value: _currentSliderValue,
-      min: 0,
-      max: 10,
-      divisions: 10,
-      label: _currentSliderValue.toString(),
-      onChanged: (double value) {
-        setState(() {
-          _currentSliderValue = value;
-          _weeklyWBScore = value;
-        });
-      },
-    );
+    return Column(children: [
+      Text("How do you feel right now?"),
+      Slider(
+        value: _currentSliderValue,
+        min: 0,
+        max: 10,
+        divisions: 10,
+        label: _currentSliderValue.toString(),
+        onChanged: (double value) {
+          setState(() {
+            _currentSliderValue = value;
+          });
+        },
+      ),
+      RaisedButton(
+          onPressed: () {
+            _weeklyWBScore = _currentSliderValue;
+          },
+          child: const Text('Done'))
+    ]);
+  }
+}
+
+//pedometer
+class PedometerWidget extends StatefulWidget {
+  @override
+  _PedometerWidgetState createState() => _PedometerWidgetState();
+}
+
+class _PedometerWidgetState extends State<PedometerWidget> {
+  Stream<StepCount> _stepCountStream;
+  String _steps = '?';
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  void onStepCount(StepCount event) {
+    print(event);
+    setState(() {
+      _steps = event.steps.toString();
+    });
+  }
+
+  void onStepCountError(error) {
+    print('onStepCountError: $error');
+    setState(() {
+      _steps = 'Step Count not available';
+    });
+  }
+
+  void initPlatformState() {
+    _stepCountStream = Pedometer.stepCountStream;
+    _stepCountStream.listen(onStepCount).onError(onStepCountError);
+
+    if (!mounted) return;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [Text("Your steps this week:"), Text(_steps)]);
   }
 }
 
@@ -69,6 +120,8 @@ Future<num> readStepCount() async {
 }
 
 */
+//notification
+/*
 FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
     FlutterLocalNotificationsPlugin();
 
@@ -122,3 +175,4 @@ Future<void> scheduleNotification() async {
       scheduledNotificationDateTime,
       platformChannelSpecifics);
 }
+*/
