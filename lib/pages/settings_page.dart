21ee7f2714767adf9d0c62e6baa6ac8391eac47c
saachipahwa <55795994+saachipahwa@users.noreply.github.com>
2020-12-28 22:02:58 +0000

    setting page basic elements finished

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -2,13 +2,98 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-_getPostcode() async {
+class SettingsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: "Settings",
+        home: Scaffold(
+            body: SafeArea(
+                child: Column(children: [
+          Text("Settings",
+              style: TextStyle(fontSize: 28.0, fontWeight: FontWeight.w700)),
+          ChangePostcodeWidget(),
+          ChangeSupportWidget()
+        ]))));
+  }
+}
+
+class ChangePostcodeWidget extends StatefulWidget {
+  @override
+  _ChangePostcodeWidgetState createState() => _ChangePostcodeWidgetState();
+}
+
+class _ChangePostcodeWidgetState extends State<ChangePostcodeWidget> {
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [Text("Postcode: ")]),
+      Row(children: [
+        FutureBuilder<String>(
+            future: _getPostcode(),
+            builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+              return new Flexible(
+                  child: TextField(
+                decoration: InputDecoration(
+                    border: InputBorder.none, hintText: snapshot.data),
+                maxLength: 4,
+                inputFormatters: [
+                  FilteringTextInputFormatter.allow(RegExp(r"[a-zA-Z0-9]+"))
+                ],
+                onChanged: (text) {
+                  setState(() {
+                    _updatePostcode(text);
+                  });
+                },
+              ));
+            })
+      ])
+    ]);
+  }
+}
+
+class ChangeSupportWidget extends StatefulWidget {
+  @override
+  _ChangeSupportWidgetState createState() => _ChangeSupportWidgetState();
+}
+
+class _ChangeSupportWidgetState extends State<ChangeSupportWidget> {
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [Text("Support Code: ")]),
+      Row(children: [
+        FutureBuilder<String>(
+            future: _getSupportCode(),
+            builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+              return new Flexible(
+                  child: TextField(
+                decoration: InputDecoration(
+                    border: InputBorder.none, hintText: snapshot.data),
+                inputFormatters: [
+                  FilteringTextInputFormatter.allow(RegExp(r"[0-9]+"))
+                ],
+                onChanged: (text) {
+                  setState(() {
+                    _updateSupportCode(text);
+                  });
+                },
+              ));
+            })
+      ])
+    ]);
+  }
+}
+
+Future<String> _getPostcode() async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
   String userPostcode = prefs.getString('postcode');
   return userPostcode;
 }
 
-_getSupportCode() async {
+Future<String> _getSupportCode() async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
   String userSupportCode = prefs.getString('support_code');
   return userSupportCode;
@@ -22,35 +107,4 @@ _updatePostcode(String value) async {
 _updateSupportCode(String value) async {
   final SharedPreferences prefs = await SharedPreferences.getInstance();
   await prefs.setString('support_code', value);
-}
-
-class SettingsWidget extends StatefulWidget {
-  @override
-  _SettingsWidgetState createState() => _SettingsWidgetState();
-}
-
-class SettingsWidgetState extends SettingsWidget {}
-
-class SettingsPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-      Row(children: [
-        Text("Postcode: "),
-        TextField(
-          decoration: InputDecoration(
-              border: InputBorder.none, labelText: _getPostcode()),
-          maxLength: 4,
-          inputFormatters: [
-            FilteringTextInputFormatter.allow(RegExp(r"[a-zA-Z0-9]+"))
-          ],
-          onChanged: (text) {
-            setState(() {
-              _currentPostcode = text;
-            });
-          },
-        ),
-      ]),
-    ]);
-  }
 }
