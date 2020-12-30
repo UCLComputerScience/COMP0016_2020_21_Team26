1a1caad6528d733d024ced84583dbf1acf26e571
saachipahwa <55795994+saachipahwa@users.noreply.github.com>
2020-12-30 12:01:37 +0000

    unused import

--- lib/pages/intro_screen.dart
+++ lib/pages/intro_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:nudge_me/main_pages.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:introduction_screen/introduction_screen.dart';
