eff7647ee181876c31f44ddd0f16213bd9882c0f
saachipahwa <55795994+saachipahwa@users.noreply.github.com>
2021-01-09 10:39:50 +0000

    Improved intro screen (#38)
    * improved intro screen
    
    * support code text field instead of multiple choice
    
    * removing main
    
    * fixing build error?
    
    * unused import
    
    * push replacement
    
    * multiple choice removed
    
    * removing input formatters\
    
    * unused import
    
    * requested changes
    
    * changing to textformfield and using validators
    
    * changing postcode validator to work always
    
    * unused import
    
    * removing bits for testin
    
    * settings page changes
    
    * save function
    
    * fix validation on intro screen
    
    * switch back to stateful and dispose of text controllers
    
    * run flutter format .
    
    * reverting to old settings page
    
    * Update lib/pages/intro_screen.dart
    
    * Update lib/pages/intro_screen.dart
    
    * removed material app
    
    Co-authored-by: thevirtuoso1973 <chris.tomy@hotmail.com>
    Co-authored-by: Chris <46009390+thevirtuoso1973@users.noreply.github.com>

--- lib/pages/intro_screen.dart
+++ lib/pages/intro_screen.dart
@@ -1,95 +1,143 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:introduction_screen/introduction_screen.dart';
 import 'package:nudge_me/background.dart';
 import 'package:nudge_me/main.dart';
 import 'package:nudge_me/main_pages.dart';
-import 'package:nudge_me/notification.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:nudge_me/notification.dart';
 
 /// Screen that displays to faciliate the user setup.
 /// Also schedules the checkup/publish notifications here to ensure that
 /// its only done once.
-class IntroScreen extends StatefulWidget {
-  @override
-  _IntroScreenState createState() => _IntroScreenState();
-}
-
-class _IntroScreenState extends State<IntroScreen> {
-  final _formKey = GlobalKey<FormState>();
-
+class IntroScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-          body: Column(
-        children: [
-          Text(
-            "NudgeMe",
-            style: TextStyle(fontSize: 25.0),
-          ),
-          Text("Welcome, let's get you started:"),
-          Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: const InputDecoration(
-                      hintText: "Enter your postcode prefix"),
-                  validator: (value) {
-                    // TODO: could improve validation
-                    if (value.length == 0 || value.length > 4) {
-                      return "The postcode prefix is the first part";
-                    }
-                    return null;
-                  },
-                  onSaved: _savePostcode,
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(
-                      hintText: "Enter your support code"),
-                  validator: (value) => null, // NOTE: I assume this is optional
-                  onSaved: _saveSupportCode,
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    if (_formKey.currentState.validate()) {
-                      _formKey.currentState.save();
-                      _finishSetup();
+    return Scaffold(body: IntroScreenWidgets());
+  }
+}
 
-                      Navigator.pushReplacement(
-                          // switch to main app
-                          context,
-                          MaterialPageRoute(builder: (context) => MainPages()));
-                    }
-                  },
-                  child: Text("Submit"),
-                )
-              ],
-            ),
-          )
-        ],
-      )),
+class IntroScreenWidgets extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _IntroScreenWidgetsState();
+}
+
+class _IntroScreenWidgetsState extends State<IntroScreenWidgets> {
+  final postcodeController = TextEditingController();
+  final supportCodeController = TextEditingController();
+
+  void _saveInput(String postcode, String suppcode) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString('postcode', postcode);
+    prefs.setString('support_code', suppcode);
+  }
+
+  bool _isInputValid(String postcode, String suppCode) {
+    return 2 <= postcode.length && postcode.length <= 4 && suppCode.length > 0;
+  }
+
+  void _onIntroEnd(context) {
+    if (!_isInputValid(postcodeController.text, supportCodeController.text)) {
+      Scaffold.of(context).showSnackBar(SnackBar(
+        content: Text("Invalid postcode or support code."),
+      ));
+      return;
+    }
+
+    _saveInput(postcodeController.text, supportCodeController.text);
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(builder: (_) => MainPages()),
     );
+    _finishSetup();
   }
 
   void _finishSetup() async {
     scheduleCheckup(DateTime.sunday, const Time(12));
     schedulePublish(DateTime.monday, const Time(12));
-
     SharedPreferences.getInstance()
         .then((prefs) => prefs.setBool(FIRST_TIME_DONE_KEY, true));
-
     // only start tracking steps after user has done setup
     initBackground();
   }
 
-  void _savePostcode(String value) async {
-    final prefs = await SharedPreferences.getInstance();
-    prefs.setString('postcode', value);
+  @override
+  Widget build(BuildContext context) {
+    const pageDecoration = const PageDecoration(
+        titleTextStyle: TextStyle(fontSize: 28.0, fontWeight: FontWeight.w700),
+        bodyTextStyle: TextStyle(fontSize: 20.0),
+        descriptionPadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
+        pageColor: Color.fromARGB(255, 251, 249, 255),
+        imagePadding: EdgeInsets.zero);
+
+    return IntroductionScreen(
+        pages: [
+          PageViewModel(
+              title: "Welcome",
+              image: Image.asset("lib/images/IntroLogo.png", height: 350.0),
+              body: "Swipe to set up",
+              decoration: pageDecoration),
+          PageViewModel(
+              title: "Postcode",
+              image: Center(
+                  child: Image.asset("lib/images/IntroPostcode.png",
+                      height: 270.0)),
+              bodyWidget: (Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("What is the first half of your postcode?",
+                        style: TextStyle(fontSize: 20.0),
+                        textAlign: TextAlign.center),
+                    TextField(
+                      controller: postcodeController,
+                      textAlign: TextAlign.center,
+                      // https://github.com/flutter/flutter/issues/67236
+                      maxLength: 4, // length of a postcode prefix
+                      decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Enter postcode here"),
+                    ),
+                  ])),
+              decoration: pageDecoration),
+          PageViewModel(
+              title: "Support",
+              image: Center(
+                  child: Image.asset("lib/images/IntroSupport.png",
+                      height: 270.0)),
+              bodyWidget: (Column(children: <Widget>[
+                Text("Where do you primarily go to find support?",
+                    style: TextStyle(fontSize: 20.0),
+                    textAlign: TextAlign.center),
+                TextField(
+                  controller: supportCodeController,
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Enter support code here"),
+                ),
+              ])),
+              decoration: pageDecoration),
+        ],
+        onDone: () => _onIntroEnd(context),
+        showSkipButton: false,
+        next: const Icon(Icons.arrow_forward,
+            color: Color.fromARGB(255, 182, 125, 226)),
+        done: const Text('Done',
+            style: TextStyle(
+                fontWeight: FontWeight.w600,
+                color: Color.fromARGB(255, 182, 125, 226))),
+        dotsDecorator: const DotsDecorator(
+            size: Size(10.0, 10.0),
+            color: Color(0xFFBDBDBD),
+            activeColor: Color.fromARGB(255, 0, 74, 173),
+            activeSize: Size(22.0, 10.0),
+            activeShape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(25.0)))));
   }
 
-  void _saveSupportCode(String value) async {
-    final prefs = await SharedPreferences.getInstance();
-    prefs.setString('support_code', value);
+  void dispose() {
+    // need to dispose of [TextEditingController]
+    postcodeController.dispose();
+    supportCodeController.dispose();
+    super.dispose();
   }
 }
