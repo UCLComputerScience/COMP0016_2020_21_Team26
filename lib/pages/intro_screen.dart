f0af9a14b2d784793844d5f67412a1647112d8b8
thevirtuoso1973 <chris.tomy@hotmail.com>
2021-01-21 11:15:23 +0000

    close keyboard when user changes page

--- lib/pages/intro_screen.dart
+++ lib/pages/intro_screen.dart
@@ -200,6 +200,12 @@ class _IntroScreenWidgetsState extends State<IntroScreenWidgets> {
             style: TextStyle(
                 fontWeight: FontWeight.w600,
                 color: Color.fromARGB(255, 182, 125, 226))),
+        onChange: (int _) {
+          FocusScopeNode currentFocus = FocusScope.of(context);
+          if (!currentFocus.hasPrimaryFocus) {
+            currentFocus.unfocus();
+          }
+        },
         dotsDecorator: const DotsDecorator(
             size: Size(10.0, 10.0),
             color: Color(0xFFBDBDBD),
