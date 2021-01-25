6fca0917a528d084f7138492ec947c339222bb83
saachipahwa <55795994+saachipahwa@users.noreply.github.com>
2021-01-25 21:53:12 +0000

    enum for notifs

--- lib/notification.dart
+++ lib/notification.dart
@@ -56,9 +56,15 @@ NotificationDetails _getSpecifics() {
   );
 }
 
+enum notifications { test, wbCheck, publish, nudge }
+
 Future scheduleNotification([tz.TZDateTime scheduledDate]) async {
-  await flutterLocalNotificationsPlugin.zonedSchedule(0, "NudgeMe",
-      'Test notification for debugging.', scheduledDate, _getSpecifics(),
+  await flutterLocalNotificationsPlugin.zonedSchedule(
+      notifications.test.index,
+      "NudgeMe",
+      'Test notification for debugging.',
+      scheduledDate,
+      _getSpecifics(),
       androidAllowWhileIdle: true,
       uiLocalNotificationDateInterpretation:
           UILocalNotificationDateInterpretation.absoluteTime);
@@ -68,7 +74,7 @@ Future scheduleNotification([tz.TZDateTime scheduledDate]) async {
 /// [int] day should be retrieved using DateTime's day enumeration
 Future scheduleCheckup(int day, Time time) async {
   await flutterLocalNotificationsPlugin.zonedSchedule(
-    1,
+    notifications.wbCheck.index,
     "Weekly Wellbeing Check",
     "Tap to report your wellbeing.",
     _nextInstanceOfDayTime(day, time),
@@ -85,7 +91,7 @@ Future scheduleCheckup(int day, Time time) async {
 /// cancels old checkup notfification and reschedules with new date and time
 /// [int] day should be retrieved using DateTime's day enumeration
 Future rescheduleCheckup(int day, Time time) async {
-  await flutterLocalNotificationsPlugin.cancel(1);
+  await flutterLocalNotificationsPlugin.cancel(notifications.wbCheck.index);
   scheduleCheckup(day, time);
 }
 
@@ -93,7 +99,7 @@ Future rescheduleCheckup(int day, Time time) async {
 /// [int] day should be retrieved using DateTime's day enumeration
 Future schedulePublish(int day, Time time) async {
   await flutterLocalNotificationsPlugin.zonedSchedule(
-    2,
+    notifications.publish.index,
     "Publish Score",
     "Tap to review and publish your weekly score anonymously.",
     _nextInstanceOfDayTime(day, time),
@@ -111,13 +117,13 @@ Future schedulePublish(int day, Time time) async {
 /// cancels old publish notfification and reschedules with new date and time
 /// [int] day should be retrieved using DateTime's day enumeration
 Future reschedulePublish(int day, Time time) async {
-  await flutterLocalNotificationsPlugin.cancel(2);
+  await flutterLocalNotificationsPlugin.cancel(notifications.publish.index);
   schedulePublish(day, time);
 }
 
 void scheduleNudge() async {
   await flutterLocalNotificationsPlugin.zonedSchedule(
-    3,
+    notifications.nudge.index,
     "Nudge",
     "Hey, your score or steps are low, want to share with a friend?",
     tz.TZDateTime.now(tz.local).add(Duration(seconds: 2)),
