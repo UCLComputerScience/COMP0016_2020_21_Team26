219437e3db5b957a637b8002d3ce7ac892aa7118
saachipahwa <55795994+saachipahwa@users.noreply.github.com>
2021-02-09 23:03:54 +0000

    fixing home page coach mark

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -40,7 +40,7 @@ class _HomePageState extends State<HomePage> {
 
   void showTutorial() async {
     if (!(await _isHomeTutorialDone())) {
-      Timer(Duration(milliseconds: 100), () => showCoachMarkWB());
+      Timer(Duration(milliseconds: 500), () => showCoachMarkWB());
     }
   }
 
@@ -62,7 +62,7 @@ class _HomePageState extends State<HomePage> {
                       "This is where you can view \n last week's score.",
                       style: Theme.of(context).textTheme.subtitle2)))
         ],
-        duration: Duration(seconds: 10),
+        duration: Duration(seconds: 8),
         onClose: () {
           Timer(Duration(milliseconds: 100), () => showCoachMarkSteps());
         });
