4b575ad7d0ea4a75cb8c32a302236180412fceb1
Chris <chris.tomy.19@ucl.ac.uk>
2021-02-23 18:42:31 +0000

    test conditional page display

--- test/pages/sharing_page_test.dart
+++ test/pages/sharing_page_test.dart
@@ -6,17 +6,61 @@ import 'package:nudge_me/pages/sharing_page.dart';
 
 import '../widget_test.dart';
 
-void main() {
-  testWidgets('displays detailed help with no friends',
-      (WidgetTester tester) async {
-    final mockedFriendDB = MockedFriendDB();
-    await tester
-        .pumpWidget(wrapAppProvider(SharingPage(), friendDB: mockedFriendDB));
+final List<Friend> friends = [
+  Friend(
+    id: 0,
+    name: 'ExampleName',
+    identifier: 'ExampleId',
+    publicKey: 'ExamplePubKey',
+    latestData: null,
+    read: null,
+    currentStepsGoal: null,
+    sentActiveGoal: 0,
+    initialStepCount: null,
+  ),
+];
 
-    expect(
-        find.byWidgetPredicate((widget) =>
-            widget is Text && widget.data.contains("texting them a link")),
-        findsOneWidget);
+void main() {
+  group('conditional page', () {
+    testWidgets('shows detailed explanation when user has no friends',
+        (WidgetTester tester) async {
+      final mockedFriendDB = MockedFriendDB();
+      when(mockedFriendDB.getFriends())
+          .thenAnswer((realInvocation) => Future.value([]));
+      when(mockedFriendDB.empty)
+          .thenAnswer((realInvocation) => Future.value(true));
+
+      await tester
+          .pumpWidget(wrapAppProvider(SharingPage(), friendDB: mockedFriendDB));
+      await tester.pumpAndSettle();
+
+      expect(
+          find.byWidgetPredicate((widget) =>
+              widget is RichText &&
+              widget.text.toPlainText().contains('When you both have NudgeMe')),
+          findsOneWidget);
+      expect(find.byType(SliverList), findsNothing);
+    });
+    testWidgets('shows list with 1 or more friends', (WidgetTester tester) async {
+      final mockedFriendDB = MockedFriendDB();
+      when(mockedFriendDB.getFriends())
+          .thenAnswer((realInvocation) => Future.value(friends));
+      when(mockedFriendDB.empty)
+          .thenAnswer((realInvocation) => Future.value(false));
+
+      await tester
+          .pumpWidget(wrapAppProvider(SharingPage(), friendDB: mockedFriendDB));
+      await tester.pumpAndSettle();
+      await tester.drag(find.text('My Identity \nCode'), Offset(0.0, -500.0));
+      await tester.pumpAndSettle();
+
+      expect(
+          find.byWidgetPredicate((widget) =>
+          widget is RichText &&
+              widget.text.toPlainText().contains('When you both have NudgeMe')),
+          findsNothing);
+      expect(find.byType(SliverList), findsOneWidget);
+    });
   });
 }
 
