9083ae463c7e2d1567f7e5fbe319758db5c46415
saachipahwa <55795994+saachipahwa@users.noreply.github.com>
2021-03-02 18:13:16 +0000

    to->by

--- lib/pages/sharing_page.dart
+++ lib/pages/sharing_page.dart
@@ -266,7 +266,7 @@ class SharingPageState extends State<SharingPage> {
                 RichText(
                     text: new TextSpan(children: [
                       new TextSpan(
-                          text: "Share the link to using one of these buttons:",
+                          text: "Share the link using one of these buttons:",
                           style: Theme.of(context).textTheme.bodyText2)
                     ]),
                     textAlign: TextAlign.center),
